d0ac38cb0e290c1d1e787225c38ad905632530fd
lindy-zhang <lwzhang@mit.edu>
2026-09-12 09:59:09 -0400

    Add split_heads and merge_heads helpers to MultiHeadAttention

--- attention/multi_head_attention.py
+++ attention/multi_head_attention.py
@@ -19,4 +19,17 @@ class MultiHeadAttention(nn.Module):
         self.w_v = nn.Linear(self.d_model, self.d_model, bias=False)
         self.w_o = nn.Linear(self.d_model, self.d_model, bias=False)
     
-   
+    def split_heads(self, x: Tensor) -> Tensor:
+        batch, seq_len, d_model = x.shape
+        # Reshape last dim into (num_heads, d_k)
+        x = x.reshape(batch, seq_len, self.num_heads, self.d_k)
+        # Swap seq_len and num_heads dims
+        x = x.transpose(-3, -2)
+        return x # (batch, num_heads, seq_len, d_k)
+
+    def merge_heads(self, x: Tensor) -> Tensor:
+        batch, num_heads, seq_len, d_k = x.shape
+        x = x.transpose(-3, -2)
+        x = x.contiguous()
+        x = x.reshape(batch, seq_len, self.d_model)
+        return x
